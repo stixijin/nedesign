5f2bb10370c8213358922d1e53c58709a546b118
stixijin <stixijin@gmail.com>
2024-05-13 20:49:41 +0300

    Animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "http://nedesigner.ru"
+    site: "http://nedesigner.ru",
+    prefetch: {
+      prefetchAll: true,
+      defaultStrategy: 'viewport'
+    }
   });
