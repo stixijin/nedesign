a1254798fcf15554aac116ed1c9fd4f75d6761f9
stixijin <stixijin@gmail.com>
2024-05-19 19:37:58 +0300

    Appearance animation for areaSwitcher

--- src/entities/areasSwitcher/ui/areaSwitcher.vue
+++ src/entities/areasSwitcher/ui/areaSwitcher.vue
@@ -9,7 +9,22 @@
         <slot name="icon"></slot>
 </button>
 
-<ul v-if="open" class="areaList">
-    <slot name="areaList"></slot>
-</ul>
+<transition name="fade">
+    <ul v-if="open" class="areaList">
+        <slot name="areaList"></slot>
+    </ul>
+</transition>
+
 </template>
+
+<style>
+    .fade-enter-active,
+    .fade-leave-active {
+    transition: opacity 0.5s ease;
+    }
+
+    .fade-enter-from,
+    .fade-leave-to {
+    opacity: 0;
+    }
+</style>
