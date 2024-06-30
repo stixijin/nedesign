2fd55617e18893afe52bea06264a41a182d39668
stixijin <stixijin@gmail.com>
2024-06-30 23:10:13 +0300

    Filling with content

---
+++ src/entities/strengths/ui/advantage.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import "./advantages.scss";
+
+
+</script>
+
+
+<template>
+
+    <li class="advantage">
+        <div class="advantage__header">
+            <h3>
+                <slot name="title"></slot>
+            </h3>
+        </div>
+        <div class="advantage__body">
+            <p>
+                <slot name="body"></slot>
+            </p>
+        </div>
+    </li>
+
+</template>
